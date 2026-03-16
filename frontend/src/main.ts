13a06b78cb3dd2a6d217f93fd53252402a09a5c2
iain <iain@iain.nl>
2026-03-16 22:43:06 +0100

    Add Global Error Handler and Unhandled Rejection Listener (#143)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,15 @@ import router from './router'
 import './style.css'
 
 const app = createApp(App)
+
+app.config.errorHandler = (err, instance, info) => {
+  console.error('[Vue error]', info, err, instance)
+}
+
+window.addEventListener('unhandledrejection', (event) => {
+  console.error('[Unhandled rejection]', event.reason)
+})
+
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
